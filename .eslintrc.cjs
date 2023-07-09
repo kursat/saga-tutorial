3360b5b0307fad37ac0c1b689ecca2b95c30c66b
Kürşat Yiğitoğlu <kursat.yigitoglu@gmail.com>
2023-07-09 17:35:36 +0300

    eslint and prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,7 @@
 /* eslint-env node */
 
+// Why I am using prettier alpha
+// https://stackoverflow.com/questions/76624993/prettier-3-0-0-typeerror-prettier-resolveconfig-sync-is-not-a-function
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
@@ -16,12 +18,13 @@ module.exports = {
     project: true,
     tsconfigRootDir: __dirname,
   },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', "prettier"],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
     '@typescript-eslint/no-non-null-assertion': 'off',
+    "prettier/prettier": "error"
   },
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 function App() {
-  return (
-      <div className="bg-cyan-600 text-blue-900">
-          Clean page
-      </div>
-  )
+  return <div className="bg-cyan-600 text-blue-900">Clean page</div>
 }
 
 export default App
